109b7fedba4a32d7a10273ddd4d7e1476c676956
Nicolas <nicolas.flores.eguizabal.25@gmail.com>
2026-04-15 23:30:52 -0600

    test

--- src/main/java/com/example/app_springboot/HelloController.java
+++ src/main/java/com/example/app_springboot/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "¡Hola Mundo";
+        return "¡Hola Mundo desde Digital Ocean! siuuuuu";
     }
 }
